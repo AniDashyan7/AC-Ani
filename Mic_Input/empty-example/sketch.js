384a29e021fac6b53e16b5ed56c3c0df05edcf18
AniDashyan7 <50113579+AniDashyan7@users.noreply.github.com>
2019-06-27 14:32:55 +0400

    Update sketch.js

--- Mic_Input/empty-example/sketch.js
+++ Mic_Input/empty-example/sketch.js
@@ -1,11 +1,9 @@
-
-
 var mic;
 var button;
 var smoothMicLevel=0;
 
 function setup(){
-  createCanvas(windowWidth,windowHeight-100);
+  createCanvas(windowWidth,windowHeight-100,WEBGL);
   button = createButton("Listen");
   button.mousePressed(toggleListen);
 	mic = new p5.AudioIn()
@@ -15,23 +13,26 @@ function setup(){
 function draw(){
   background(0);
   micLevel = mic.getLevel();
-  translate(width/2,height/2);
+  console.log(micLevel); 
+  translate(width/20,height/20);
 
   smoothMicLevel = lerp(smoothMicLevel, micLevel,0.5);
-  fill(255,0,0);
-  // stroke(255);
+  fill(random(255),random(255),random(255));
+  noStroke();
 
-  beginShape();
-  for(var a = 0; a < TWO_PI;a+=0.01){
-    var r = width/10;
+  for(var i = 0; i <  50; i++){
+    beginShape();
+    for(var a = 0; a < TWO_PI;a+=0.01){
+    var r = width/5;
     var x = smoothMicLevel * r * 16 *  pow(sin(a), 3);
     var y = smoothMicLevel*(-r*(13 * cos(a) - 5*cos(2*a) - 2*cos(3*a) - cos(4*a)));
-    vertex(x,y);
+    ellipse(x*i,y*i,i,i);
   }
-  endShape();
+    endShape();
+  }
+    
 
-  // var vol = mic.getLevel();
-  // ellipse(width/2,height/2,vol*width,vol*width);
+   
 }
 
 function toggleListen() {
