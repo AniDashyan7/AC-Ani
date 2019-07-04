d5c5ba0b7659a28393176a0b4821d05fbe13c8b4
AniDashyan7 <50113579+AniDashyan7@users.noreply.github.com>
2019-07-04 18:53:01 +0400

    Add files via upload

---
+++ mymusicapp/src/app/hippo/hippo.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hippo',
+  templateUrl: './hippo.page.html',
+  styleUrls: ['./hippo.page.scss'],
+})
+export class HippoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
